45a777912dd4c8e1e838e4de55a06470abe5cc7b
leozin777 <leo.cardoso@rede.ulbra.br>
2022-10-19 18:28:21 -0300

    aula de hj

---
+++ Tde-1910.sql
@@ -0,0 +1,35 @@
+create table categorias
+(
+    id int auto_increment primary key,
+    nome varchar(50) not null
+)
+
+create table fornecedores
+(
+    id int auto_increment primary key,
+    nome varchar(50) not null
+)
+
+create table produtos
+(
+    id int auto_increment primary key,
+    id_categoria int not null,
+    descricao varchar(300) not null,
+    data_cadastro date not null,
+    valor_unitario int not null,
+    constraint produto_fk_produto_tem_categorias
+    foreign key(id_categoria) references(categorias),
+)
+
+create table pedidos
+(
+    id int auto_increment primary key,
+    id_fornecedor int,
+    id_produto int,
+    valor_unitario int not null,
+    data_pedido date not null,
+    constraint pedido_fk_pedido_tem_fornecedor
+    foreign key(id_fornecedor) references(fornecedores),
+    constraint pedido_fk_pedido_possui_produto
+    foreign key(id_produto) references(produtos)
+)
---
+++ tde.sql
@@ -0,0 +1,76 @@
+--TDE DDL
+
+create table colaboradores
+(
+    id int not null auto_increment primary key,
+    num_matricula int not null unique,
+    nome varchar(100) not null,
+    email varchar(50) not null,
+    senha varchar(100) not null,
+    id_coordenador int,
+    constraint coordenadores_fk_colaboradores
+    foreign key(id_coordenador) references colaboradores(id)
+);
+
+create table projetos
+(
+    id int not null auto_increment primary key,
+    nome varchar(100) not null,
+    descricao varchar(500) not null,
+    duracao float not null,
+    id_colaborador int not null,
+    constraint fk_projeto_tem_colaborador
+    foreign key(id_colaborador) references colaboradores(id)
+);
+
+create table projetos_tem_colaboradores
+(
+    id int auto_increment primary key,
+    id_projeto int not null,
+    id_colaborador int not null,
+    constraint projetos_fk_projetos_tem_colaboradores
+    foreign key(id_projeto) references projetos(id),
+    constraint colaboradores_fk_projetos_tem_colaboradores
+    foreign key(id_colaborador) references colaboradores(id)
+);
+
+
+create table projetos_comentarios
+(
+    id int not null auto_increment primary key,
+    descricao varchar(200) not null,
+    data_hora datetime not null, 
+    id_colaboradorer int not null,
+    id_projeto int not null,
+    constraint fk_comentario_feito_colaborador
+    foreign key(id_colaborador) references colaboradores(id),
+    constraint comentarios_fk_projetos
+    foreign key(id_projeto) references projetos(id)
+);
+
+create table departamentos
+(
+    id int auto_increment primary key,
+    nome varchar(100) not null
+);
+
+create table projetos_melhoria_processos
+(
+    id int auto_increment primary key,
+    resultado_esperado varchar(100) not null,
+    id_projeto int not null,
+    id_departamento int not null,
+    constraint projetos_fk_projetos_melhoria_processo
+    foreign key(id_projeto) references projetos(id),
+    constraint departamentos_fk_projetos_melhoria_processo
+    foreign key(id_departamento) references departamento
+);
+
+create table projetos_sociais 
+(
+    id int auto_increment primary key,
+    publico_beneficiario varchar(200) not null,
+    id_projeto int not null,
+    constraint projetos_fk_projetos_sociais
+    foreign key(id_projeto) references projetos(id)
+);
